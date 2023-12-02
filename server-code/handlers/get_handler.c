676f3a0d04d3eae9188c9183ec946db9026c6817
Nitin <ntnbhat9@gmail.com>
2023-12-02 16:35:59 -0500

    added versioned get command

--- server-code/handlers/get_handler.c
+++ server-code/handlers/get_handler.c
@@ -16,6 +16,7 @@ void get_controller(char *client_message , int client_sock){
     // Extract the remote path and file content from the command:
     char command[10];   // Variable for "GET command"
     char filename[100];  // Variable for "filename"
+    char version[20]; // version information
 
     // Use sscanf to extract the values
     if (sscanf(client_message, "%s %s\n", command, filename) != 2) {
@@ -23,22 +24,32 @@ void get_controller(char *client_message , int client_sock){
         return 1;
     }
 
+    char *versionStart = strchr(client_message, '\n');
+
     char full_remote_path[1024];
     snprintf(full_remote_path, sizeof(full_remote_path), "server-file-system/%s", filename);
 
-    handle_get_command(client_sock , full_remote_path);
+
+    if (versionStart == NULL) {
+        // simple case where you do not specify any specific version
+        handle_get_command(client_sock , full_remote_path ,  NULL);
+    }else{
+        // case in which you specify a specific version
+        versionStart++;  // Move to the first character after the newline
+
+        // Copy the version character by character
+        size_t i = 0;
+        while (versionStart[i] != '\0') {
+            version[i] = versionStart[i];
+            i++;
+        }
+        version[i] = '\0';  // Null-terminate the version
+        handle_get_command(client_sock , full_remote_path , version);
+    }
 
 }
 
-void handle_get_command(int client_sock, const char *remote_path) {
-
-    char server_message[8196];
-    char client_response[8196];
-
-    // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_response, '\0', sizeof(client_response));
-
+char* findLatestVersion(const char *remote_path){
     // Extract directory path and filename
     char dir_path[256];
     char filename[256];
@@ -80,7 +91,55 @@ void handle_get_command(int client_sock, const char *remote_path) {
     // Copy the new path to remote_path
     strcpy(remote_path, new_remote_path);
 
+    return remote_path;
+} 
 
+const char* findSpecificVersion(const char *remote_path , const char* version){
+    // Extract directory path and filename
+    char dir_path[256];
+    char filename[256];
+
+    strncpy(dir_path, remote_path, sizeof(dir_path));
+    strncpy(filename, remote_path, sizeof(filename));
+
+    char *dirname_result = dirname(dir_path);
+    char *basename_result = basename(filename);
+
+    // Get the current working directory
+    char base_directory[256];
+    if (getcwd(base_directory, sizeof(base_directory)) == NULL)
+    {
+            perror("Error getting current working directory");
+            return;
+    }
+    char new_remote_path[1024];
+    char versioned_filename[256];
+    snprintf(versioned_filename, sizeof(versioned_filename), "%s_v%s", basename_result, version);
+    snprintf(new_remote_path, sizeof(new_remote_path), "%s/%s", dirname_result, versioned_filename);
+
+    strcpy(remote_path, new_remote_path);
+
+    return remote_path;
+} 
+
+
+void handle_get_command(int client_sock, const char *remote_path , const char* version) {
+
+    char server_message[8196];
+    char client_response[8196];
+
+    // Clean buffers:
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_response, '\0', sizeof(client_response));
+
+    //update the remote path with specific version
+    if(version == NULL){
+        strcpy(remote_path, findLatestVersion(remote_path));
+    }else{
+        strcpy(remote_path, findSpecificVersion(remote_path , version));
+    }
+
+    
     // Read the content from the remote file:
     FILE *remote_file = fopen(remote_path, "rb");
 
